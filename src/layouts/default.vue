8d491735b85bc2690961ceaa9e5be6921560d0ee
linjiangl <8257796@qq.com>
2024-10-11 13:15:51 +0800

    页面布局配置

---
+++ src/layouts/default.vue
@@ -0,0 +1,37 @@
+<template>
+  <wd-config-provider :themeVars="themeVars">
+    <slot />
+    <wd-toast />
+    <wd-message-box />
+    <wd-tabbar
+      v-model="tabBarIndex"
+      fixed
+      active-color="#ee0a24"
+      inactive-color="#7d7e80"
+    >
+      <wd-tabbar-item
+        is-dot
+        :value="2"
+        title="点状"
+        icon="home"
+      ></wd-tabbar-item>
+      <wd-tabbar-item :value="2" icon="cart" title="分类"></wd-tabbar-item>
+      <wd-tabbar-item :value="30" title="我的" icon="user"></wd-tabbar-item>
+      <wd-tabbar-item :value="200" title="最大值" icon="photo"></wd-tabbar-item>
+      <wd-tabbar-item :value="10" title="客服" icon="chat"></wd-tabbar-item>
+    </wd-tabbar>
+  </wd-config-provider>
+</template>
+
+<script lang="ts" setup>
+import type { ConfigProviderThemeVars } from "wot-design-uni";
+import { ref } from "vue";
+
+const themeVars: ConfigProviderThemeVars = {
+  // colorTheme: "red",
+  // buttonPrimaryBgColor: "#07c160",
+  // buttonPrimaryColor: "#07c160",
+};
+
+const tabBarIndex = ref<number>(0);
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,45 +1,10 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import AppFooter from '@/components/AppFooter.vue'
-import AppLogos from '@/components/AppLogos.vue'
-import InputEntry from '@/components/InputEntry.vue'
-
-
+import { ref } from "vue";
+import AppFooter from "@/components/AppFooter.vue";
+import AppLogos from "@/components/AppLogos.vue";
+import InputEntry from "@/components/InputEntry.vue";
 
 const aa = 1;
-
-const current = ref<number>(0);
-const tabBar = [
-  {
-    pagePath: '/pages/index',
-    text: 'code',
-    iconPath: '/static/images/tabbar/home.png',
-    selectedIconPath: '/static/images/tabbar/home-active.png'
-  },
-  {
-    pagePath: '/pages/about',
-    text: 'extend',
-    iconPath: '/static/images/tabbar/writing.png',
-    selectedIconPath: '/static/images/tabbar/writing-active.png'
-  },
-  {
-    pagePath: '/pages/login/index',
-    text: 'thor',
-    iconPath: '/static/images/tabbar/user.png',
-    selectedIconPath: '/static/images/tabbar/user-active.png',
-    num: 2,
-    isDot: true,
-    verify: true
-  }
-]
-
-function tabbarSwitch(e) {
-  current.value = e.index;
-
-  // uni.navigateTo({
-  //   url: tabBar[current.value]['pagePath'],
-  // })
-}
 </script>
 
 <template>
@@ -47,6 +12,5 @@ function tabbarSwitch(e) {
     <AppLogos />
     <InputEntry />
     <AppFooter />
-    <tui-tabbar :current="current"  backdropFilter backgroundColor="#f8f8f8" :tabBar="tabBar" color="#777" selectedColor="#AC9157" @click="tabbarSwitch"></tui-tabbar>
   </view>
 </template>
